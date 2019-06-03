fdf766935548b4ece39a4dd033be8c5cba6edcb9
Tadynas <griciustadas@gmail.com>
2019-06-03 12:09:15 +0300

    Add dictionary iteration

--- Collections/dictionaries.py
+++ Collections/dictionaries.py
@@ -44,6 +44,65 @@ del ted["job"]
 print(ted)
 #{'first_name': 'Ted', 'last_name': 'Greek', 'editor': 'any'}
 
+#Dictionary iteration
+course_minutes = {"Python Basics": 232, "Django Basics": 237, "Flask Basics": 189, "Java Basics": 133}
 
-#Packing and Unpacking dictionaries
-print(">>>Packing and Unpacking dictionaries<<<")
+for course in course_minutes:
+    print(course)
+#Python Basics
+#Django Basics
+#Flask Basics
+#Java Basics
+
+for course in course_minutes:
+    print(course_minutes[course])
+#232
+#237
+#189
+#133
+
+for key in course_minutes.keys():
+    print(key)
+#Python Basics
+#Django Basics
+#Flask Basics
+#Java Basics    
+
+for value in course_minutes.values():
+    print(value)
+#232
+#237
+#189
+#133
+
+for item in course_minutes.items():
+    print(item)
+#('Python Basics', 232)
+#('Django Basics', 237)
+#('Flask Basics', 189)
+#('Java Basics', 133)
+
+def word_count(text):
+    text = text.lower()
+    words = text.split()
+    print(words)
+    dictionary = {}
+    for word in words:
+        dictionary[word] = 0
+    for word in words:
+        dictionary[word] += 1
+    return dictionary
+
+print(word_count("I love the way I am"))
+#{'i': 2, 'love': 1, 'the': 1, 'way': 1, 'am': 1}
+
+def word_count_second(text):
+    words = text.lower().split()
+    dictionary = {}
+    for word in words:
+        dictionary[word] = words.count(word)
+    return dictionary
+
+
+print(word_count_second("As soon as possible"))
+#{'as': 2, 'soon': 1, 'possible': 1}
